100658a3a5ec241b97712e575d7de7adf670da2c
eslam ehab <eslamaboutaleb@yahoo.com>
2026-09-25 19:56:19 +0300

    fix: ensure body has exact same ids and root wrapper uses contents to maintain exact DOM tree

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import './globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  
     <Providers>
       <App />
       <Toaster
@@ -22,5 +22,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         }}
       />
     </Providers>
-  </React.StrictMode>
+  
 );
